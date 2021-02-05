4185ab65bfc2827b5a8c1ed03123d6eab30db6e7
William Bugenis <wbugenis@gmail.com>
2021-02-05 16:13:08 -0500

    commit tree 8b5ad7f7352b368e73dbf2ca327306003070addd

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,4 +20,25 @@ class ArticlesController < ApplicationController
   end
 
   # add edit and update methods here
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy 
+    Article.find(params[:id]).destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
 end
